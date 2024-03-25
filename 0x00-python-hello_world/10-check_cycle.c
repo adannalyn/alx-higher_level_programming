e23ef60bc0b0c982242457cf97881f181989ada1
adannalyn <qvexvon@gmail.com>
2024-03-25 06:58:29 -0700

    Easter eggs

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+  * check_cycle - checks if ths singly linked list has a circle
+  * list: header to a pointer
+  */
+int check_cycle(listint_t *list)

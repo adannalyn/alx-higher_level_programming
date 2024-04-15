e13e940e33014c4fc8ac93f52fa32eebc7fd0f4a
adannalyn <qvexvon@gmail.com>
2024-04-15 09:10:15 -0700

    if else lower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def islower(c):
+    ord(c)
     if c in {'a','b','c','d','e','f','g','h','i','j','k','l','m','n',
              'o','p','q','r','s','t','u','v','w','x','y','z'}:
         return True

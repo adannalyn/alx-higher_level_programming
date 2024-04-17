3066a5afcdd1107b825ad2e7b3c7c0c50e94814e
adannalyn <qvexvon@gmail.com>
2024-04-17 03:30:52 -0700

    if else lower

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-    result = ""
+    result = ''
     for words in str:
         if ord('a') <= ord(words) <= ord('z'):
             results = chr(ord(words) - 32)

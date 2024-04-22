d6a40cf651b4517143ccf1774a35d844278a221e
adannalyn <qvexvon@gmail.com>
2024-04-21 21:20:27 -0700

    modules

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    pass
+    if idx < 0:
+        return None
+    elif idx not in my_list:
+        return None
+    else:
+        index = my_list.index(idx)
+        return index
+
 
 if __name__ == "__main__":
     pass

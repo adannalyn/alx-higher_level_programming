11a20a4e2bb3e8dc634c00029b52348b6f64f05e
adannalyn <qvexvon@gmail.com>
2024-04-23 10:36:36 +0100

    lists

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    new_liste = my_list.copy()
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
+        return my_list
+    else:
+        new_liste[idx] = element
+    return new_liste
+
+
+
+if __name__ == "__main__":
+    pass

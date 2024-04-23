6bb087705df08b66c80bf6b1262680fbb9dcab11
adannalyn <qvexvon@gmail.com>
2024-04-23 04:39:27 +0100

    lists

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    for inte in len(my_list).reverse():
+        print("{:d}".format(inte))
+        return inte
+
+
+if __name__ == "__main__":
+    pass

a2ba314d0263d9eee138b05289db3669c2d2de47
adannalyn <qvexvon@gmail.com>
2024-04-25 04:32:03 +0100

    modules

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,12 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
+        first = True
         for num in row:
-            if len(num) < 3:
-                print("{}".format(num), end=" ")
+            if not first:
+                print(" ", end="")
+            print("{}".format(num), end="")
+            first = False
         print()
 
 

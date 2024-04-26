5565feb784ed1dcfb7841d2ac0010f64118a1242
adannalyn <qvexvon@gmail.com>
2024-04-26 21:24:08 +0100

    tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    
-    result = tuple(tuple_a + tuple_b for tuple_a, tuple_b in zip(tuple_a,  tuple_b))
-    
-    print("{}".format(result))
-    
+
+    a = tuple_a + (0, 0) [:2 - len(tuple_a)]
+    b = tuple_b + (0, 0) [:2 - len(tuple_b)]
+
+    first = a[0] + b[0]
+    second = a[1] + b[1]
+
+    return (first, second)
+
     if __name__ == "__main__":
         pass

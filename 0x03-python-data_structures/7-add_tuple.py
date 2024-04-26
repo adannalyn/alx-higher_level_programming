3e09d5fca68bfbe44590083c1135cd6f90fa9b65
adannalyn <qvexvon@gmail.com>
2024-04-26 21:27:29 +0100

    tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,8 +2,8 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
 
-    a = tuple_a + (0, 0) [:2 - len(tuple_a)]
-    b = tuple_b + (0, 0) [:2 - len(tuple_b)]
+    a = tuple_a + (0, 0)[:2 - len(tuple_a)]
+    b = tuple_b + (0, 0)[:2 - len(tuple_b)]
 
     first = a[0] + b[0]
     second = a[1] + b[1]

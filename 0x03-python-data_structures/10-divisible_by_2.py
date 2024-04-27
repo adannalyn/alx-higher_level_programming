fe3e283bb34ea645b0298477b6815798d6e5e01d
adannalyn <qvexvon@gmail.com>
2024-04-27 06:04:25 +0100

    max number

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    num = my_list.copy()
+    for i in num:
+        if i % 2 == 0:
+            return True
+
+
+if __name__ == "__main__":
+    pass

eac7063f8be1d50299be53895fa39951808cdd2b
adannalyn <qvexvon@gmail.com>
2024-04-28 15:08:17 +0100

    Lists Matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    pass
+    num_list = []
+    for row in matrix:
+        for num in row:
+            num_list.append(num * 2)
+    return num_list
 
 
 if __name__ == "__main__":

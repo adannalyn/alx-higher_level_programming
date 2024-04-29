ada86452f2c28551d94ab5d7d2dbed2ab7806835
adannalyn <qvexvon@gmail.com>
2024-04-29 03:52:07 -0700

    lists

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    #my_list = [replace if num == search else num for num in my_list]
-
     new_list = []
-    for num in range(len(my_list)):
-        if num == index(search):
+    for num in my_list:
+        if num == search:
             new_list.append(replace)
         else:
             new_list.append(num)

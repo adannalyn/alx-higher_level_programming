08c4b40d50c15d41100aee8caf9229e75f120132
adannalyn <qvexvon@gmail.com>
2024-04-29 11:14:04 +0100

    Lists Matrix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    my_list = [replace if num == search else num for num in my_list]
-    return my_list
+    #my_list = [replace if num == search else num for num in my_list]
+
+    new_list = []
+    for num in range(len(my_list)):
+        if num == index(search):
+            new_list.append(replace)
+        else:
+            new_list.append(num)
+    return new_list
 
 
 if __name__ == "__main__":

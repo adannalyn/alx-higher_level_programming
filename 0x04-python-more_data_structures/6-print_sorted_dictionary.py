722b422adf7d49b618e3b14d9ee1195c09f0d7eb
adannalyn <qvexvon@gmail.com>
2024-04-30 14:42:29 +0100

    Sorted Dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,7 +2,7 @@
 
 def print_sorted_dictionary(a_dictionary):
     for k, v in sorted(a_dictionary.items()):
-        print(k, v)
+        print("{}: {}".format(k, v))
 
 
 if __name__ == "__main__":

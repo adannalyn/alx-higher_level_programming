82f62e9b67d13276bc474aece20fa132e5a52567
adannalyn <qvexvon@gmail.com>
2024-05-04 07:33:36 -0700

    Multiply values

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    value = list(a_dictionary.values())
+    key = list(a_dictionary.keys())
+
+    if a_dictionary == "":
+        return None
+
+    k[value.index(max(value))]
+    return max_key
+
+
+if __name__ == "__main__":
+    pass

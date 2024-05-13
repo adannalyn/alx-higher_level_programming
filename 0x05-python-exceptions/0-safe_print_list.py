be0619295d4f706bbbe70b934288c7ad086598d9
adannalyn <qvexvon@gmail.com>
2024-05-13 05:11:57 -0700

    print n elements

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+        count = 0
+        try:
+                for i in range(x):
+                        element = my_list[i]
+                        count += 1
+                        print(element, end="")
+        except IndexError:
+                pass
+        print()
+        return count

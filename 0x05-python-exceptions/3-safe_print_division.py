4f45ebf2112c1d5260a9081a51d0e32335b121db
adannalyn <qvexvon@gmail.com>
2024-05-13 13:15:42 -0700

    print an integer division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     results = 0
     try:
         if a == 0 or b == 0:
-            return None
+            return
         else:
             result = int(a) / int(b)
             results = "{}".format(result)

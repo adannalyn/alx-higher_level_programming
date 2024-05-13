3663824129fc2fc2b09d3485f81854e98bb7ec3e
adannalyn <qvexvon@gmail.com>
2024-05-13 13:42:13 -0700

    print an integer division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,12 +2,9 @@
 def safe_print_division(a, b):
     results = 0
     try:
-        if a == 0 or b == 0:
-            return None
-        else:
-            result = int(a) / int(b)
-            results = "{}".format(result)
-            return results
+        result = int(a) / int(b)
+        results = "{}".format(result)
+        return results
     except ZeroDivisionError:
         pass
     finally:

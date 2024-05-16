0f47a6c4781375b79a8d97d62c1fe714f55aa3ac
adannalyn <qvexvon@gmail.com>
2024-05-16 02:24:36 -0700

    Class Squares

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,8 +3,23 @@
 
 
 class Square:
+    """A class representing a square"""
+
     def __init__(self, size):
+        """
+        Initialize size with a given size
+
+        Parameters:
+            size: the size
+        """
         self.__size = size
+
     def get_size(self):
+        """
+        Accessing a private instance
+
+        Return:
+            size: the variable
+        """
         return self.__size
 square = Square(6)

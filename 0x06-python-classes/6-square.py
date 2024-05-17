f63d8a44e3ea1434f8e93de8d1591d00d8dfb47c
adannalyn <qvexvon@gmail.com>
2024-05-16 19:13:43 -0700

    Spaces and Hashtags

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -32,16 +32,10 @@ class Square:
     @position.setter
     def position(self, value):
         """set property of size"""
-        if not isinstance(value, tuple):
+        if (not isinstance(value, tuple) or len(value) != 2 or
+            not all(num >= 0 for num in value) or
+            not all(isinstance(num, int) for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
-        if len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if not isinstance(value[0], int):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if not isinstance(value[1], int):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if value[0] < 0 or value[1] < 0:
-            raise ValueError("size must be >= 0")
         self.__position = value
 
     def area(self):

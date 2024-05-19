1befdeaf002e95a1e85890ae8c81b64048745d03
adannalyn <qvexvon@gmail.com>
2024-05-19 15:12:56 -0700

    matrix division by div

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -7,7 +7,7 @@ def matrix_divided(matrix, div):
     newMatrix = []
     for row in matrix:
         if len(matrix[0]) != len(matrix[1]):
-            raise TypeError("matrix must have each row with the same size")
+            raise TypeError("Each row of the matrix must have the same size")
         newrow = []
         for element in row:
             if not isinstance(element, (int, float)):

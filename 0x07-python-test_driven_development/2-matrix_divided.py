02199cf8dfa7f2f967bf23df19868e8ba1cb7623
adannalyn <qvexvon@gmail.com>
2024-05-19 15:09:40 -0700

    matrix division by div

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,8 +6,14 @@ def matrix_divided(matrix, div):
     """Divide a matrix by an integer"""
     newMatrix = []
     for row in matrix:
+        if len(matrix[0]) != len(matrix[1]):
+            raise TypeError("matrix must have each row with the same size")
         newrow = []
         for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            if not isinstance(div, (int, float)):
+                raise TypeError("div must be a number")
             result = round(element / div, 2)
             newrow.append(result)
         newMatrix.append(newrow)

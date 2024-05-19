d62731044ed1e7b31feaf714429c035700353259
adannalyn <qvexvon@gmail.com>
2024-05-19 11:40:00 -0700

    matrix division by div

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,7 +5,7 @@
 def matrix_divided(matrix, div):
     """Divide a matrix by an integer"""
     if len(matrix[0]) != len(matrix[1]):
-        raise TypeError("matrix must have each row with the same size")
+        raise IndexError("matrix must have each row with the same size")
     newMatrix = []
     for row in matrix[0:]:
         newrow = []

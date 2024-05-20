40c2251f435e126d75af97b60355d4e0dc38c5fe
adannalyn <qvexvon@gmail.com>
2024-05-20 12:48:34 -0700

    Spring test

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,11 +6,10 @@ def text_indentation(text):
     """Punctuation mark"""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    text = text.strip()
-    n = '.\n\n'
-    o = '?\n\n'
-    p = ':\n\n'
-    text = text.replace('.', n).replace('?', o).replace(':', p)
-    print()
-    for strips in text.split('\n'):
-        print(strips.strip())
+    alpha = ""
+    for char in text:
+        alpha += char
+        alpha.strip()
+        if char in ['.', '?', ':']:
+            alpha += '\n\n'
+    print(alpha.strip())

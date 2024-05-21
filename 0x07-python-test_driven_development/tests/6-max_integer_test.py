7a7dee468888c2e11974b1aac4783ed67a950207
adannalyn <qvexvon@gmail.com>
2024-05-21 07:05:14 -0700

    unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -9,6 +9,14 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1, 2, 3 ,4 , 5]), 5)
     def test_max_integer_beginning(self):
         self.assertEqual(max_integer([5, 1, 2, 3, 4]), 5)
+    def test_max_integer_middle(self):
+        self.assertEqual(max_integer([1, 2, 5, 3, 4]), 5)
+    def test_one_negative(self):
+        self.assertEqual(max_integer([-1, 2, 3 ,4 ,5]), 5)
+    def test_one_integer(self):
+        self.assertEqual(max_integer([5]), 5)
+    def test_empty(self):
+        self.assertIsNone(max_integer[], None)
 
 if __name__ == "__main__":
     unittest.main()

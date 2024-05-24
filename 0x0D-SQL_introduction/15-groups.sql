e8d525d4c5348a20a5ecbd43073072f1ca54440f
adannalyn <qvexvon@gmail.com>
2024-05-24 04:16:01 -0700

    bobs double

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
-11;rgb:0000/0000/0000-- list same scores under number
+-- list same scores under number
 SELECT score, COUNT(score) AS number
 FROM second_table
 GROUP BY score;
-ORDER BY score, number DESC;
+ORDER BY number DESC;

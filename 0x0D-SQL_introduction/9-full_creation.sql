27954c92c228bdb230d3542783e5dbc936077d9b
adannalyn <qvexvon@gmail.com>
2024-05-23 22:23:20 -0700

    second table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,4 +6,4 @@ name VARCHAR(256),
 score INT
 );
 INSERT INTO second_table (id, name, score) VALUES(1, "John", 10),
-(1, "Alex", 3), (3, "Bob", 14), (4, "George", score = 8)
+(2, "Alex", 3), (3, "Bob", 14), (4, "George", 8)
